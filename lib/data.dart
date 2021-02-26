fcc1dc01f8ec73756d11331a798103b67a1f46e5
GAUTAMSHETA <78837362+GAUTAMSHETA@users.noreply.github.com>
2021-02-27 00:21:18 +0530

    Add files via upload

--- lib/data.dart
+++ lib/data.dart
@@ -1,1 +1,120 @@
+class CodingCrossword{
+  final String name = "CODING CROSSWORD";
+  final String poster = "Image/In/CodingCrossword_In1.jpg";
+  final String discription = "An event that has an interesting crossword to solve through basic concepts covered in the  initial years of Computer Engineering.For the next round, you’ll need to solve snippets of code and find the output for those to get to the answers of crossword.\n\nEvent Date: 16th October 2020\nEvent Time: 8:30pm to 10:30am\nEvent Platform: Hackerearth\nParticipation Mode- Single\n\nEvent Rules:\nThis is a single round event which is completely online.\nTime Duration is of 2 hours with 20 Images as Problem Statements";
+  final String organizer = "Feny Bhutwala\nDhvani Shah";
+  final String TeamSize = "2";
+  final String eventtype = "Technical";
+  final String register = "you can register solo or in team";
+}
+
+class LogoCodify{
+  final String name = "LOGOCODIFY";
+  final String poster = "Image/In/LogoCodify_In1.png";
+  final String discription = "LogoCodify is a technical event and it has two rounds:\n\nRound 1- In the first round, participants have to combine the broken pieces of logo and note the matrix containing numbers written over the broken pieces.They then have to search and highlight this matrix in a much bigger table given at the end and they also have to type in the name of the logo in google forms.\nRound 2(If shortlisted from Round 1)- In the second round, participants have to shorten the length of given code while keeping the given input and output same.\n\nEvent Date: 15th October 2020\n\nEvent Time: \nRound 1: 11:30 am to 12:30 am\nRound 2: 2:00 pm to 3:00 pm\nEvent Platform: (Link will be provided) in W.A Group\nParticiation Mode: Single\n\nEvent Rules:\nRound 1: Submit the answers in given time limit.\nRound 2: Input and Output should not be changed.";
+  final String organizer = "Jaini Jariwala\nMaitry Gajiwala\nHeta Bhalala";
+  final String TeamSize = "1";
+  final String eventtype = "Technical";
+  final String register = null;
+}
+
+class DecodersHunt{
+  final String name = "DECODER'S HUNT";
+  final String poster = "Image/In/DecodersHunt_In1.png";
+  final String discription = "An event where you will solve basic coding questions, presented to you with a twist.The second round will take you on a journey where at each station you will have to solve a pseudo code before you reach the destination.\n\nEvent Date: 15 October 2020\nEvent Time: Round 1: 10:00 am to 11:30 am\nRound 2: 2:30 pm to 4:30 pm\nEvent Platform : Google Forms, Google Classroom, Google Meet\nParticipation Mode: Single\n\nRound 1:\nIn the first round, participants have to solve the problems of logical reasoning and core understandingof coding. The questions will be encrypted. First decrypt the question and then solve the problem.Thiswill be time and score based qualifier round.The participants having maximum correct answers in minimum time will be qualified for the second round.\nRound 2: \nAt the beginning of the round the participants will be given a clue that will lead them to a problem set.Each problem set will have a question to solve and a clue to the next problem set. After reaching the final problem set, the participants will have to write a code from the data collected during this code-hunt. This is a time based round.Top 3 participants with correct answer in minimum time will be declared winners.\n\nEvent Rules:\n1. Participants will have to submit the answers through google forms only.\n2.Each participant can submit the form once only. Repeated entries will not be considered.\n3.Top entries with the maximum score in minimum time will be qualified for the second round.\nThose who are qualified for the second round will be contacted.\n4. A password-protected pdf with details will be shared at the beginning of second round.\n5.Both the rounds will be conducted separately.";
+  final String organizer = "Aakash Nandwani\nMeet Patel\nRutvi Sutariya\nFaizan Mombasawala";
+  final String TeamSize = "1";
+  final String eventtype = "Technical";
+  final String register = null;
+}
+
+class Rangolify{
+  final String name = "RANGOLIFY";
+  final String poster = "Image/In/Rangolify_In1.jpg";
+  final String discription = "The aim for this event is to create Designs using Python turtle module.\n\nEvent Date: 15 October 2020(Round 1)\nEvent Time: 1:30 to 2:30pm(Day 1)\nEvent Platform: Google Classroom and Google Quiz\nParticipation Mode: Single or Team of 2\n\nEvent Rules: \nRangolify is a 2 rounds event. Round 1 is a quiz on turtle module of python. Shortlisted entries will go to round 2 where they have to code for the design provided.";
+  final String organizer = "Nency Badiyani\nVishesh Darji";
+  final String TeamSize = "2";
+  final String eventtype = "Technical";
+  final String register = "you can register solo or in team";
+}
+
+class Codemania{
+  final String name = "CODEMANIA";
+  final String poster = "Image/In/CodeMania_In1.png";
+  final String discription = "So you think you can code? eh!..try your hands at the Ultimate Competitive Programming Battle.Love competitive programming? Get ready to show your skills in the coding race.\n\nEvent Date: 15th October\nEvent Time: 8:00pm to 12:00am\nEvent Platform: Hackerearth\nParticipation Mode: Single Participation\n\nEvent Rules:\nThis is a single round event which is completely online.Time Duration is of 4 hours with 6 Problem StatementsThere will be multiple questions of varying difficulties.Top 3 contestants will be on the podium of CODEMANIA.Any submissions after the allotted time will not be considered.No extra time will be provided for late entries.";
+  final String organizer = "Hardik Shah\nYashvi Malu";
+  final String TeamSize = "1";
+  final String eventtype = "Technical";
+  final String register = null;
+}
+
+class GameChefer{
+  final String name = "GAMECHEFER";
+  final String poster = "Image/In/GameChefer_In.jpg";
+  final String discription = "Wanna play a game? Yess! You can but let's make it first\nRound 1 - develop any kind of logical game (can be any small logical game like sudoku but should be innovative)Top 5 game would be selected for next round (innovation main factor)\nRound 2: it's play time, yess! One will play remaining 4 game and will put their reviews verbally in a Google meet (kind of debate)\nConsidering the correction of reviews and level of game to level of boggle of mind winners would be selected\n\nEvent Date: 15th-16th October 2020\nEvent Platform: Google form/Google classroom \nEvent Time:\nRound 1: 8:30 am to 3:30 pm(15th October)(first round is submission round,just you have to submit your game code anytime in the given time interval)\nRound 2: 1:30 pm to 2:30 pm(16th October)\nParticipation Mode: Single Participation\n\nEvent Rules:\n1. Participants can code in any language or use any platform of their choice\n2. The time duration will start as soon as the registration process terminates.\n3. The code should be ready 5 minutes before the submission time.\4. The theme of the game should be innovative and creative.\n5. Plagiarism will not be tolerated. Those participants who are caught will be disqualified.(Required time)\n6.For second round Reviews of games(other’s) should be actual.";
+  final String organizer = "Hiral Rathod\nNisha Kakadiya";
+  final String TeamSize = "4";
+  final String eventtype = "Technical";
+  final String register = "you can register solo or in team";
+}
+
+class Abhivyakti{
+  final String name = "ABHIVYAKTI";
+  final String poster = "Image/In/Abhivyakti_In1.png";
+  final String discription = "Abhivyakti is a 3 round event emphasized on Diction skills as well as critical thinking. This event will brush up the skills that are required while giving a speech, presenting a presentation, having a debate and also engaging in a conversation.\nRound 1 is a riddle round. If selected round 2 is a Debate Round and further if shortlisted round 3 is about Podcast Talk.\n\nEvent Date: 15th October\nEvent Time: 9:30 to 10:00am(Round 1)\nEvent Platform: Google Forms and Google Meet\nParticipation Mode: Team of 2\n\nEvent Rules:\nThis is a three round event which is completely online.Time Duration for the 1st round is 30 minutes and is all about solving riddles and brain teasers. Participants must join the provided platforms at given time. Joining information and redirect links will be provided later once registered. Details about round 2 and 3 will be discussed with the selected participants.";
+  final String organizer = "Urja Rayajiwala\nAnas kunda";
+  final String TeamSize = "2";
+  final String eventtype = "Technical";
+  final String register = "you have to register in team";
+}
+
+class CodeTheHints{
+  final String name = "CODE THE HINTS";
+  final String poster = "Image/In/CodeTheHints_In1.jpeg";
+  final String discription = "How about a game where coding is too simple but what matters is your observation. Observe the images,Collect the Hints, and Code!!\n\nEvent Date: 16th October 2020\nEvent Time: 8:30pm to 10:30am\nEvent Platform: Hackerearth\nParticipation Mode- Single\n\nEvent Rules:\nThis is a single round event which is completely online.\nTime Duration is of 2 hours with 20 Images as Problem Statements";
+  final String organizer = "Akabari Darshita\nSheta Gautam";
+  final String TeamSize = "1";
+  final String eventtype = "Technical";
+  final String register = null;
+}
+
+class Innotech{
+  final String name = "INNOTECH";
+  final String poster = "Image/In/Innotech_In1.png";
+  final String discription = "This event is basically related to innovative, business or any startup ideas. In this event the participants will get a platform to present any innovative ideas that they have, may it be in terms of any innovative business idea or any unique project that they have in their mind.\n\nEvent Date: 15 October 2020(Round 1)\nEvent Time: 8:30 to 2:30pm(Round 1)You can submit the Entries any time in this interval.\nEvent Platform: Google Classroom and Google Meet\nParticipation Mode: Team of 3\n\nEvent Rules: \nInnotech is a 3 round event. For round 1, participants will have to submit the abstract of their idea in the classroom in form of pdf (Link of classroom will be provide after successfull registration) . Selection ofthe teams would be done on the basis of innovative and unique idea. All participants willreceive 2 E-certificate.One certificate from college and another from the company.If selected for round 2, participants would have to present their ideas to jury members virtually. On the basis of your presentation and idea, participants will be selected for next round. For round 3 participants have to make innovative marketing posters or video's for marketing purpose.Timings and details for round 2 and 3 would be provided when got selected.";
+  final String organizer = "Soni Nimisha\nBhavik Jinjala";
+  final String TeamSize = "3";
+  final String eventtype = "Non-Tech";
+  final String register = "you can register solo or in team";
+}
+
+class Scavenger{
+  final String name = "SCAVENGER HUNT";
+  final String poster = "Image/In/ScavengerHunt_In1.jpg";
+  final String discription = "Scavenger Hunt is a non-technical event,In this game Puzzles will be given to participants. They have to solve it and find Answer(object or place) and click the picture of that and upload it. All the answer(object or place) will easily available at home, so participants Don’t need to go outside.\n\nEvent Date: 16 October 2020\nEvent Time: Round 1: 11:00 am to 12:30 am\nEvent Platform: Google Form/Google classroom \nParticipation Mode: Single\n\nEvent Rules:\nOnly original pictures are valid. Don’t send pictures from google or anywhere.Time will be considered. i.e. The participant, who will solve puzzles in less Time will be consider as a winner.";
+  final String organizer = "Nisha Rana\nNidhi Patel";
+  final String TeamSize = "1";
+  final String eventtype = "Non-Tech";
+  final String register = null;
+}
+
+class Valorant{
+  final String name = "VALORANT";
+  final String poster = "Image/In/Valorant_In1.jpg";
+  final String discription = "A 5v5 character-based tactical FPS,PC game where precise gun play meets unique agent abilities. where one team attacks and the other defends.The attacking team’s goal is to plant a bomb (called a spike) and have it detonate.\n\nEvent Date: 15-16 October 2020\nEvent Time: 8:30 to 5:00pm(Day 1)\nEvent Time: 8:30 to 2:30pm(Day 2)\nEvent Platform: Meet and Valorant\nParticipation Mode: Team of 5\n\nEvent Rules:\nParticipants must have the riot Id of all members,Discord";
+  final String organizer = "Prem Goswami\nJanam Shah\nAnuj Bhagat\nKeyur Joshi";
+  final String TeamSize = "5";
+  final String eventtype = "Non-Tech";
+  final String register = "you have to register in team";
+}
+
+class TheWatchCraft{
+  final String name = "THE WATCH CRAFT";
+  final String poster = "Image/In/TheWatchCraet_In1.jpg";
+  final String discription = "The Event is Non-Technical and the Event is on Streaming Movies and TV-Series (Binge Watching).We will have questions based on Popular Movies & TV-Series such as Dialogues, Characters, Intro, Music, Casts, etc.We will have 3 Rounds and after the progression of each round the number of participants is reduced to half and the three fastest performers in Round 3 will be declared as a Winners.Event Type: Single Participation\n\nEvent Date: 15 October 2020\nEvent Time: \nRound 1: 8:30 am to 11:00 am\nRound 2: 1:30 pm to 3:00 pm\nRound 3: 4:00 pm to 5:00 pm\nEvent Platform: Website(Link will be provided)\n\nEvent Rules :\nRound 1: This round will be open for all participants for 30 minutes. You can login during this time and answer the quiz question as mention. you have 2 minutes to answer the given questions.More score and least time consider first.50% of participants will go for the next round .\nDetails of Round 2 and 3 will be provided later to selected ones.";
+  final String organizer = "Keyur Vadodariya\nSmit Dhameliya\nNikhil Maguwala\nPrashant Godhani";
+  final String TeamSize = "1";
+  final String eventtype = "Non-Tech";
+  final String register = null;
+}
 
